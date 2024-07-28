ac0d23eb5c740197919c1789c1da8f22a444fb4e
yash patel <yp87844@gmail.com>
2024-07-28 20:20:07 +0530

    changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -21,11 +21,11 @@ const LandingPage = () => {
           <h2 className="text-3xl xl:text-5xl font-semibold max-w-[500px] mt-36 mb-4 xl:leading-[70px] xl:text-black">
             Find your Dream Property Now
           </h2>
-          <p className=" mb-8 xl:text-black max-w-[500px]">
+          {/* <p className=" mb-8 xl:text-black max-w-[500px]">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type.
-          </p>
+          </p> */}
           <div className="flex">
             <div className="flex justify-center  gap-3 items-center absolute">
               <button className="bg-[#325131] cursor-pointer text-white px-4 py-3 border-none rounded-lg">
